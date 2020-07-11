c87bd5bc62f68dba07e5f5b78f2fb8ae55c789fc
Rodrigo Nunes <rnsribeiro@gmail.com>
2020-07-10 22:05:40 -0300

    adicionado arquivo userinfo.lua com informações do usuário

---
+++ userinfo.lua
@@ -0,0 +1,23 @@
+-- Created on 10 de julho de 2020
+
+-- @author: Rodrigo Nunes
+
+--[[
+	Retorna a chave do usuário que deve ser definida no site 
+	mercadobitcoin.net essa chave não deve ser tornado publica
+	por questões de segurança. ]]--
+KEY = "c3defcf6d4aa97f331e32d6651906687"
+
+--[[ 
+	Retorna o codigo do usuario que deve ser definido no
+	site mercadobitcoin.net esse codigo nao deve ser
+	tornado publico por questoes de seguranca. ]]--
+CODE = "721d1a318a41f926231344cbb0b5db4c3dc1f100c50e073a76be998f4a1eb806"
+
+--[[ 
+	Retorna o codigo pin do usuario que deve ser definido no
+	site mercadobitcoin.net esse codigo noo deve ser
+	tornado publico por questoes de seguranca. ]]--
+PIN = "4298"
+
+
