e9d2f4b76628e52ac0bd4d52eb87335c20239720
ansribeiro <arnaldonunessampaio@gmail.com>
2020-07-24 10:11:31 -0300

    Criado a class ListOrderBook em list_orderbook

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from cancel_order import CancelOrder
-from place_sell_order import PlaceSellOrder
+#from cancel_order import CancelOrder
+#from place_sell_order import PlaceSellOrder
+from list_orderbook import ListOrderBook
 import time
 
-sell = PlaceSellOrder("BRLXRP",0.1,1.081)
-time.sleep(10)
+#sell = PlaceSellOrder("BRLXRP",0.1,1.081)
+#time.sleep(10)
 #
-print(sell.getOrderId())
-cancel = CancelOrder("BRLXRP",sell.getOrderId(),str(int(time.time())))
+#print(sell.getOrderId())
+#cancel = CancelOrder("BRLXRP",sell.getOrderId(),str(int(time.time())))
+
+listOrderBook = ListOrderBook("BRLXRP",str(int(time.time())))
+print(listOrderBook.getOrderbookAsksID())
+
